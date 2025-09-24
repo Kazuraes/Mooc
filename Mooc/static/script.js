67f789b7d9b219b2e4ef77f31a684a9a9691e738
Almo720 <mokashfikabashi@gmail.com>
2025-09-24 23:00:13 +0200

    Update script.js

--- Mooc/static/script.js
+++ Mooc/static/script.js
@@ -1,1 +1,13 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.querySelector('.button')
+    const bouton = document.querySelector('.bouton')
+    const ouvriere = document.querySelector('.creer_une_page_html')
+    bouton.addEventListener('click', () => {
+        ouvriere.style.display = 'flex'
+        button.style.display = 'block'
+})
+   button.addEventListener('click', () => {
+    ouvriere.style.display = 'none'
+    button.style.display = 'none'
+   })
+})
