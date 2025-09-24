88c1f21047ae5c442bbbb009024ff1c279c378a3
Almo720 <mokashfikabashi@gmail.com>
2025-09-24 22:57:46 +0200

    Update app.py

--- Mooc/app.py
+++ Mooc/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request,redirect, url_for,session # on importe les fonctions flask necessaire
+from flask import Flask, render_template, request,redirect, url_for, session # on importe les fonctions flask necessaire
 import json, os # on importe aussi la fonction json
 app = Flask(__name__) # on fait une variable qui contient Flask
 fichier = 'utilisateur.json' # on donne au variable fichier le contenu json
@@ -6,10 +6,10 @@ def lectureJson() : # on fait une fonction qui :
     if os.path.exists(fichier) : # verifie si le fichier existe
         with open(fichier, 'r', encoding='utf-8') as f : # si il existe bien alors on l'ouvre mode lecture
             return json.load(f) # transforme le JSON en dictionnaire Python
-@app.route('/page') # on fait une route flask avec le mots cle @app.route et on donne un nom
+@app.route('/creer_une_page_html') # on fait une route flask avec le mots cle @app.route et on donne un nom
 # ⚠ c'est obligatoire pour pouvoir aller sur une page
-def page() : 
-    return render_template('page.html') # fonction qui retourne la page.html ici il faut mettre rendre_template
+def creer_une_page_html() : 
+    return render_template('creer_une_page_html.html') # fonction qui retourne la page.html ici il faut mettre rendre_template
 @app.route('/') # ici c'est la page principal index.html qu'on l'utilisateur entre dans le site
 def home() : 
 
@@ -32,7 +32,7 @@ def inscription() :
             utilisateurs = [] # on fait une liste utilisateurs
         if any(u['email'] == email for u in utilisateurs) : # verifier les doublons si l'email est deja present
             return render_template('index.html', error='cette email est deja utiliser !') # alors renvoie une erreur
-        # voir index.html la syntexte suivante {% if error %} si erreur alors paragraphe rouge indiquant l'ereur
+        # voir index.html la syntexte suivante (qui est du Jinja2) {% if error %} si erreur alors paragraphe rouge indiquant l'ereur
         # s'il y a pas de doublons on fait un dictionnaire qui contiens le prenom, email et mot de passe
         nouvelle_utilisateur = {
             'prenom' : prenom,
@@ -44,7 +44,7 @@ def inscription() :
             json.dump(utilisateurs, f, indent=4, ensure_ascii=False) # on ajoute utilisateurs dans le json
            # ensure_ascii=False : permet d’afficher les caractères spéciaux (é, à, 漢字…) sans les convertir en \uXXXX
            # ensure_ascii=True  (valeur par défaut) : convertit tout caractère non-ASCII en séquence \uXXXX
-    return redirect(url_for('page'))# des que tout est envoier email prenom password alors on renvoie vers la page.html
+    return redirect(url_for('creer_une_page_html'))# des que tout est envoier email prenom password alors on renvoie vers la page.html
 @app.route('/login', methods=['GET', 'POST']) # ici c'est pour la connexion methods post et get
 def connexion() : 
     if request.method == 'POST' : # si la method post alors
@@ -57,24 +57,25 @@ def connexion() :
         for utilisateur in utilisateurs : # on parcours tout le json
             # si l'email est pareil a un email dans le json et que password est pareil a un passxord json alors
             if utilisateur.get('email') == email and utilisateur.get('password') == password :
-                return redirect(url_for('page')) # alors on renvoie vers page.html
+                return redirect(url_for('creer_une_page_html')) # alors on renvoie vers creer_une_page_html.html
         return render_template('login.html', error='Email ou mot de passe incorrect') # si un dés 2 n'est pas bon alors erreur
     return render_template('login.html') # et on affiche la page login.html
-@app.route('/creer_une_page_html') # route vers creer_une_page_html
-def creer_une_page_html() : 
-    return render_template('creer_une_page_html.html.html') # on renvoie vers la creer_une_page_html.html
 
 @app.route('/organiser_votre_texte') # route vers organiser_votre_texte
-def organiser_votre_texte () : 
-    return render_template('organiser_votre_texte.html') # on renvoie vers la paorganiser_votre_texte.html
+def organiser_votre_texte() : 
+    return render_template('organiser_votre_texte.html') # on renvoie vers la organiser_votre_texte.html
 
 @app.route('/creer_un_lien_hypertexte_en_html') # route vers creer_un_lien_hypertexte_en_html
 def creer_un_lien_hypertexte_en_html() : 
     return render_template('creer_un_lien_hypertexte_en_html.html') # on renvoie vers la creer_un_lien_hypertexte_en_html.html
+
+@app.route('/inserer_des_images') # route vers inserer_des_images
+def inserer_des_images() : 
+    return render_template('inserer_des_images.html') # on renvoie vers la inserer_des_images.html
 # if __name__ == "__main__" : exécute ce code seulement si on lance ce fichier directement (pas en import)
-@app.route('/inserer_des_images_en_html') # route vers inserer_des_images_en_html
-def inserer_des_images() :
-    return render_template('inserer_des_images.html')
+@app.route('/page4')
+def page4() :
+    return render_template('page4.html')
 
 if __name__ == '__main__' : 
     # app.run(debug=True) : démarre le serveur Flask en mode debug (rechargement auto + erreurs détaillées)
